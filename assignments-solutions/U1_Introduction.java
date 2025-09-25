d6e8058e348c56d4fb6cd10cf29e3c3110939530
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-09-25 18:15:59 +0530

    Added

---
+++ assignments-solutions/U1_Introduction.java
@@ -0,0 +1,79 @@
+class U1_Introduction {
+
+    public static void main(String[] args) {
+
+
+        // 1. WAP(Write A Program) to print the the Hey, How are you on the scree.
+
+        // System.out.println("Hey, How are you");
+
+
+        
+        // 2. WAP to print your name, course, and college name on separate lines.
+
+        // System.out.println("Name = Sushdnahu Kumar");
+        // System.out.println("Course = BCA");
+        // System.out.println("College = Shri Ram College");
+
+
+
+        // 3. WAP to print "Hello, " in the first line and "How are you" in the second
+        // line using scape sequence.
+
+        // System.out.println("Hello, \nHow are you");
+
+
+
+        // 4. WAP to print "HowAreYou" on the screen (ensure the double quotes are printed).
+
+        // System.out.println(" \"HowAreYou\" ");
+        
+
+
+        // 5. WAP to print \n on the screen
+
+        // System.out.println("\\n");
+
+
+
+        // 6. WAP to print the \r on the screen
+
+        // System.out.println("\\r");
+
+
+
+        // 7. WAP to print the "Teacher's Day" with single quote and double quote.
+
+        // System.out.println(" \" Teacher's Day\" ");
+
+
+
+        // 8. WAP to calculate the size of byte, char, int, long, float, double, short.
+
+        // System.out.println("Size of byte: " + Byte.BYTES + " bytes");
+        // System.out.println("Size of char: " + Character.BYTES + " bytes");
+        // System.out.println("Size of int: " + Integer.BYTES + " bytes");
+        // System.out.println("Size of long: " + Long.BYTES + " bytes");
+        // System.out.println("Size of float: " + Float.BYTES + " bytes");
+        // System.out.println("Size of double: " + Double.BYTES + " bytes");
+        // System.out.println("Size of short: " + Short.BYTES + " bytes");
+
+
+
+        // 9. WAP to with one char type variable. Assign 'A' to it, then change its value to 'B' using the increment operator. 
+
+        // char letter = 'A';
+        // System.out.println("Value of letter variable: " + letter);
+        // int newletter = ( (int) letter + 1);
+        // System.out.println("Value of letter variable after one increment: " + ((char) newletter) );
+
+
+
+        // 10. WAP to print the ASCII value of "I".
+
+        // char letter = 'I';
+        // int inInteger = letter;
+        // System.out.println("I in integer ASCII: " + inInteger);
+
+    }
+}
